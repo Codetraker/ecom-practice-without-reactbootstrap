d58122759faa0b0b97639db420c4f5d0f4e427f4
Codetraker <swarajbibhuprasad@gmail.com>
2024-04-03 01:24:17 +0530

    structure of website

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,8 +1,17 @@
+import './ListItem.css';
+
 const ListItems = props =>{
     return (
-        <>
-            <div>Hello</div>
-        </>
+        <div className="main">
+            <div className="head">Album 1</div>
+            <div className="pic">
+                <div className='picFrame'></div>
+            </div>
+            <div className="priceAndCartbtn">
+                <div className="price">$12.99</div>
+                <button className="cartbtn">Add To Cart</button>
+            </div>
+        </div>
     );
 };
 export default ListItems;
--- src/components/MusicSec.js
+++ src/components/MusicSec.js
@@ -4,10 +4,24 @@ import './MusicSec.css';
 const MusicSec = props =>{
     return (
         <>
-            <div className="musicbar">Music</div>
+            <div className="musicbar">MUSIC</div>
             <div className='listdiv'>
                 <ListItems />
                 <ListItems />
+                <ListItems />
+                <ListItems />
+            </div>
+            <div className="merchbar">MERCH</div>
+            <div className='listdiv'>
+                <ListItems />
+                <ListItems />
+            </div>
+            <div className='seeCart'>
+                <button> See the Cart</button>
+            </div>
+            <div className='footer'>
+                <h7>The Generics</h7>
+                <div></div>
             </div>
         </>
     );
